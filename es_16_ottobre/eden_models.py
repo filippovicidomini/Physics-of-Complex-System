84a6fb063d55e5e111be3c2e32fc0b64e7471dc7
Filippo Vicidomini <filippovicidomini@gmail.com>
2023-10-17 14:42:09 +0200

    nel perculation model aggiunta funzione che conta il numero di cluster. nel jupiter rispettivo viene visualizzato il numero di cluster in funzione della probabilità che viene scelta

--- es_16_ottobre/eden_models.py
+++ es_16_ottobre/eden_models.py
@@ -1,14 +1,14 @@
 # simulation of eden's model with a single cell
 
-import numpy as np
 import random
+
 import matplotlib.pyplot as plt
-
-
+import numpy as np
 
 
 def single_neighbour(lattice, neighbour, cell):
-    # give a cell that is just occupied and return the neighbour of that cell that are free and add to the list of neighbours, then remove the double neighbours
+    # give a cell that is just occupied and return the neighbour of that cell that are free and add to the list of
+    # neighbours, then remove the double neighbours
     if lattice[cell[0] - 1][cell[1]] == 0:
         neighbour.append([cell[0] - 1, cell[1]])
 
@@ -24,38 +24,37 @@ def single_neighbour(lattice, neighbour, cell):
     return neighbour
 
 
-
-
 def eden_model(lattice=np.zeros((100, 100)), occupided=[], p=0.5, steps=10000, photo=4, candidate=[]):
+    global neighbour
     if occupided == []:
         occupided.append([lattice[0].size // 2, lattice[0].size // 2])
         lattice[lattice[0].size // 2][lattice[0].size // 2] = 1
         neighbour = single_neighbour(lattice, [], occupided[0])
-    #print(lattice)
-    #print(neighbour)
+    # print(lattice)
+    # print(neighbour)
 
     for i in range(steps):
         if random.random() < p:
-
-            #print(neighbour[0])
+            # print(neighbour[0])
             new = random.choice(neighbour)
             lattice[new[0]][new[1]] = 1
             neighbour.remove(new)
             occupided.append(new)
             neighbour += single_neighbour(lattice, [], new)
-            #print(neighbour)
-
+            # print(neighbour)
 
     plt.imshow(lattice, cmap="gray")
     # print the neighbours in different color to see if the algorithm works
     for i in range(len(neighbour)):
         plt.scatter(neighbour[i][1], neighbour[i][0], color="red", s=3)
+
+
+    plt.xticks([])
+    plt.yticks([])
+    plt.figure(figsize=(16, 14), dpi=300)
     plt.tight_layout()
     plt.show()
 
-
-    print(lattice)
-    print(neighbour)
     return lattice
 
 
--- es_16_ottobre/perculation.py
+++ es_16_ottobre/perculation.py
@@ -1,23 +1,56 @@
 # doing side perculation on a 2D lattice
 
-import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.animation as animation
 import random
-import time
+
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 p = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6]
 L = 100
+
+
 def perculation_side(p, lattice=np.zeros((100, 100))):
     for i in range(lattice[0].size):
         for j in range(lattice[0].size):
             if random.random() < p:
                 lattice[i][j] = 1
+    cluster = count_clusters(lattice)
     return lattice
+
+
+def count_clusters(matrix):
+    """Count the number of clusters in a matrix
+    explain how: https://stackoverflow.com/questions/46759492/how-to-count-the-number-of-clusters-in-a-matrix
+    """
+    def dfs(row, col):
+        if row < 0 or row >= len(matrix) or col < 0 or col >= len(matrix[0]) or matrix[row][col] != 1:
+            return
+        matrix[row][col] = -1  # Mark the cell as visited
+        for dr, dc in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+            dfs(row + dr, col + dc)
+
+    count = 0
+    for row in range(len(matrix)):
+        for col in range(len(matrix[0])):
+            if matrix[row][col] == 1:
+                count += 1
+                dfs(row, col)
+
+    # Reset marked cells back to 1
+    for row in range(len(matrix)):
+        for col in range(len(matrix[0])):
+            if matrix[row][col] == -1:
+                matrix[row][col] = 1
+
+    return count
+
+print(count_clusters(perculation_side(0.2, np.zeros((100, 100)))))
+
 
 plt.rc('text', usetex=True)
 plt.rc('font', family='serif')
+
 # 6 grafici per ogni p
 
 fig, ax = plt.subplots(2, 3, figsize=(16, 14), dpi=300)
